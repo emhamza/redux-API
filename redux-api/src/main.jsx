ba3227c49b4edbd7cc8282d0c8affa59f88d8419
emhamza <imumairhamza@gmail.com>
2023-05-10 20:32:34 +0500

    Connet React and Redux with API

--- redux-api/src/main.jsx
+++ redux-api/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux';
 import store from './app/store.js';
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
